01735f60f03cfc6ece78d75e25300c23d4e3ebcf
Misenpai <misenpai200231@gmail.com>
2024-03-13 00:07:29 +0530

    Step-1.1

--- app/src/main/java/com/example/splitit/Fragment/otpverification.kt
+++ app/src/main/java/com/example/splitit/Fragment/otpverification.kt
@@ -1,22 +1,124 @@
 package com.example.splitit.Fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.splitit.R
+import com.example.splitit.home
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
-class otpverification : Fragment() {
+class otpverification() : Fragment(), Parcelable {
+
+    private lateinit var pincodeNumber: EditText
+    private lateinit var onCodeSentValue: String
+    private lateinit var resendCode: TextView
+    private lateinit var phoneNumber: EditText
+
+    constructor(parcel: Parcel) : this() {
+
+    }
+
+    constructor(pincodeEnterEditText: EditText,onCodeSentValue:String,resendCode:TextView,phoneNumber:EditText) : this() {
+        this.pincodeNumber = pincodeEnterEditText
+        this.onCodeSentValue = onCodeSentValue
+        this.resendCode = resendCode
+        this.phoneNumber = phoneNumber
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_otpverification, container, false)
+        val view = inflater.inflate(R.layout.fragment_otpverification, container, false)
+        val proceedOTPbtn = view.findViewById<Button>(R.id.proceedOTPbtn)
+        proceedOTPbtn.setOnClickListener {
+            val pinCodeLength = pincodeNumber.text.toString().trim()
+            if (!pinCodeLength.isEmpty()) {
+                if (pinCodeLength.length == 6){
+                    if (onCodeSentValue!=null){
+                        val phoneAuthCredential = PhoneAuthProvider.getCredential(onCodeSentValue,pinCodeLength)
+                        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential)
+                            .addOnCompleteListener{task ->
+                                if (task.isSuccessful){
+                                    val intent = Intent(requireContext(),home::class.java)
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                                    startActivity(intent)
+                                }else{
+                                    Toast.makeText(requireContext(), "Enter the correct OTP", Toast.LENGTH_SHORT).show()
+
+                                }
+                            }
+                    }else{
+                        Toast.makeText(requireContext(), "Please Check Internet Connection", Toast.LENGTH_SHORT).show()
+                    }
+                    Toast.makeText(requireContext(), "OTP verified", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(requireContext(), "Please Enter all numbers", Toast.LENGTH_SHORT).show()
+                }
+
+            } else {
+                Toast.makeText(requireContext(), "OTP cannot be empty", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        resendCode.setOnClickListener{
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + phoneNumber.text.toString(),60, TimeUnit.SECONDS,requireActivity(),
+                object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                    override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                    }
+
+                    override fun onVerificationFailed(p0: FirebaseException) {
+                    }
+
+                    override fun onCodeSent(
+                        p0: String,
+                        p1: PhoneAuthProvider.ForceResendingToken
+                    ) {
+                        onCodeSentValue = p0
+                        Toast.makeText(requireContext(), "OTP send successfully", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+            )
+        }
+        return view
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<otpverification> {
+        override fun createFromParcel(parcel: Parcel): otpverification {
+            return otpverification(parcel)
+        }
+
+        override fun newArray(size: Int): Array<otpverification?> {
+            return arrayOfNulls(size)
+        }
     }
 }
--- app/src/main/java/com/example/splitit/phoneVerification.kt
+++ app/src/main/java/com/example/splitit/phoneVerification.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.splitit.Fragment.otpverification
 import com.example.splitit.databinding.ActivityPhoneVerificationBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class phoneVerification : AppCompatActivity() {
 
@@ -17,6 +22,7 @@ class phoneVerification : AppCompatActivity() {
     private lateinit var pinText: TextView
     private lateinit var resendText: TextView
     private lateinit var pincodeEnter : EditText
+    private lateinit var phoneNumber: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val binding : ActivityPhoneVerificationBinding by lazy{
@@ -31,21 +37,55 @@ class phoneVerification : AppCompatActivity() {
         pinText = binding.textView9
         resendText = binding.textView10
         pincodeEnter = binding.editTextNumber2
+        phoneNumber = binding.editTextPhone3
         binding.sendOTPbtn.setOnClickListener{
-            showOTPVerificationFragment()
-            pinFrameLayout.isEnabled = true
-            pinFrameLayout.findViewById<TextView>(R.id.textView9).isEnabled = true
-            pinFrameLayout.findViewById<TextView>(R.id.textView10).isEnabled = true
-            pinFrameLayout.findViewById<EditText>(R.id.editTextNumber2).isEnabled = true
-            pinFrameLayout.setBackgroundResource(R.drawable.phone_no_background)
-            pinText.setTextColor(ContextCompat.getColor(this,R.color.white))
-            resendText.setTextColor(ContextCompat.getColor(this,R.color.white))
-            pincodeEnter.setTextColor(ContextCompat.getColor(this,R.color.white))
+
+            if (!phoneNumber.text.toString().trim().isEmpty()){
+                if ((phoneNumber.text.toString().trim()).length == 10){
+
+
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        "+91" + phoneNumber.text.toString(),60, TimeUnit.SECONDS,this,
+                        object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                                Toast.makeText(this@phoneVerification, "Phone Number Verified", Toast.LENGTH_SHORT).show()
+                            }
+
+                            override fun onVerificationFailed(p0: FirebaseException) {
+                                Toast.makeText(this@phoneVerification, p0.message, Toast.LENGTH_SHORT).show()
+                            }
+
+                            override fun onCodeSent(
+                                p0: String,
+                                p1: PhoneAuthProvider.ForceResendingToken
+                            ) {
+                                showOTPVerificationFragment(p0)
+                                pinFrameLayout.isEnabled = true
+                                pinFrameLayout.findViewById<TextView>(R.id.textView9).isEnabled = true
+                                pinFrameLayout.findViewById<TextView>(R.id.textView10).isEnabled = true
+                                pinFrameLayout.findViewById<EditText>(R.id.editTextNumber2).isEnabled = true
+                                pinFrameLayout.setBackgroundResource(R.drawable.phone_no_background)
+                                pinText.setTextColor(ContextCompat.getColor(this@phoneVerification,R.color.white))
+                                resendText.setTextColor(ContextCompat.getColor(this@phoneVerification,R.color.white))
+                                pincodeEnter.setTextColor(ContextCompat.getColor(this@phoneVerification,R.color.white))
+                            }
+
+                        }
+
+                    )
+
+                }else{
+                    Toast.makeText(this, "Please enter correct number", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Enter phone number", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 
-    private fun showOTPVerificationFragment() {
-        val otpVerificationFragment = otpverification()
+    private fun showOTPVerificationFragment(onCodeSentValue:String) {
+        val otpVerificationFragment = otpverification(pincodeEnter,onCodeSentValue,resendText,phoneNumber)
         supportFragmentManager.beginTransaction()
             .replace(fragmentContainer.id,otpVerificationFragment)
             .commit()
